5d4e56aaf740d9a8d92d8a03ac83ff171bbdbaab
Danil Anikin <danilanikin02@icloud.com>
2026-07-15 21:53:59 +1000

    Initial Commit

---
+++ SnakeApp/SnakeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeAppApp.swift
+//  SnakeApp
+//
+//  Created by Данил on 15/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct SnakeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
